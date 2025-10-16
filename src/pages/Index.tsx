270793713aa3021877ca9f855d7ffe646139a78f
Felix Remmel <paraplu@cordonbleu.fritz.box>
2025-10-16 23:04:58 +0200

    add rules link

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -36,6 +36,7 @@ const Index = () => {
         <div className="container mx-auto px-4 py-6">
           <h1 className="text-3xl font-bold text-center">Krosser Karl IX - Roadbook</h1>
           <p className="text-center text-primary-foreground/90 mt-1">13 Bahnen</p>
+          <p className="text-center"><a href="https://drive.google.com/file/d/1or9aaxn1gqz4mEtUjl_RsVMjGE5TrnEo/view" className="text-info">&gt;Regeln&lt;</a></p>
         </div>
       </header>
 
